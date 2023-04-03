4bc8e1e6cfd42e09ed09e497ef7d1ed3c3a9313f
渣渣超cc <48849110+ZZCHao9@users.noreply.github.com>
2023-04-03 12:55:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '冯小妹',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oybRk6vAVSwytANRU3NhaxU2GwFw',
+      id: 'oybRk6i0Mb5y-xn9tdYc5K0TjXTs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'aZEnJQadLLqxSive1AuSR903xS9WEw4IG6ruX1corzY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
