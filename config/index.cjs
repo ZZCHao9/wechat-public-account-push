4ecbec2e947aad209b4e3d477b92222921fa1542
渣渣超cc <48849110+ZZCHao9@users.noreply.github.com>
2023-04-03 12:56:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -49,13 +49,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'aZEnJQadLLqxSive1AuSR903xS9WEw4IG6ruX1corzY',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '傅超',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oybRk6vAVSwytANRU3NhaxU2GwFw',
     }
   ],
 
