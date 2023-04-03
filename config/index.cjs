931b8b071bc896a4f2072b64f0b73cbd22d56980
渣渣超cc <48849110+ZZCHao9@users.noreply.github.com>
2023-04-03 11:20:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -27,7 +27,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oybRk6vAVSwytANRU3NhaxU2GwFw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '4KDfSRfo9CvohbXzKjOhVRmLpMSbeSpkmCqAU8wMe5o',
+      useTemplateId: 'wpIMncjbW_u7BkKbypEe4mRROJHpr8_Yi-1ymhFsEuY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-08',
       festivals: [
