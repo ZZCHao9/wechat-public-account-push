d97975efdf4139d143cd99589ff4c65bc2f9a699
渣渣超cc <48849110+ZZCHao9@users.noreply.github.com>
2023-04-03 11:11:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -16,6 +16,10 @@ const USER_CONFIG = {
   PROVINCE: '四川',
   CITY: '成都',
 
+  SWITCH:{
+    earthyLoveWords: true,
+  },
+  
   USERS: [
     {
       // 想要发送的人的名字
@@ -23,7 +27,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oybRk6vAVSwytANRU3NhaxU2GwFw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'yIETSC862iLITFcdFWYMo1LRt37koffQnTGw5WzRTRw',
+      useTemplateId: 'RMIdJiWrNj0XgK_Gyf9ePf6xrKzfKmFJ510NUTKSRTI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-08',
       festivals: [
@@ -31,12 +35,8 @@ const USER_CONFIG = {
         {
           type: '*生日', name: '宝贝', year: '1996', date: '06-24',
         },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '08-08',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2020', date: '04-22',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
